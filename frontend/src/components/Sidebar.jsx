3ef77f119663bba623455be2451d178e18c54cd2
Jennifer <jxnnivo@gmail.com>
2026-09-09 14:51:54 -0400

    Add Sidebar component and layout

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <nav>
+      <h2>Study Assistant</h2>
+      <ul>
+        <li>Upload</li>
+        <li>Chat</li>
+        <li>Quiz</li>
+        <li>Settings</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Sidebar
